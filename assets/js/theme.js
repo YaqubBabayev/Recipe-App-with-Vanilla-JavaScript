2fe92df3c0e8e024b8653c8c938222896a3f5899
YaqubBabayev <yaqub.babayev.1996@gmail.com>
2024-10-16 22:51:25 +0400

    Added theme switching and session save support

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -5,4 +5,23 @@
 
 "use strict";
 
+const/** {NodeElement} */ $HTML = document.documentElement;
+const/** {Boolean} */isDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
 
+if(sessionStorage.getItem("theme")) {
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+}else{
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+}
+
+let /**{Boolean} */ isPressed = false;
+
+const changeTheme = function () {
+
+}
+
+window.addEventListener("load",function () {
+    const /** {NodeElement} */ $themeBtn = document.querySelector("[data-theme-btn]");
+    $themeBtn.addEventListener("click",changeTheme);
+});
+ 
